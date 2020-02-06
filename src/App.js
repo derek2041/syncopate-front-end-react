1ef63bcd8dbf3e4a826df4b83f18adce27d8a2ac
root <root@ip-172-31-22-191.us-east-2.compute.internal>
2020-02-06 03:42:37 +0000

    Fixed syntax

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ const App = () => {
             return (
               <MessageWindow />
             );
-          />
+          }}/>
 
           <Route path="/chat" exact render={() => {
             return (
