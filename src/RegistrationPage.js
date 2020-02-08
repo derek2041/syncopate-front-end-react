bcd9b59b020c74cf885771defe77886172270c09
zhan2539 <zhan2539@purdue.edu>
2020-02-08 15:57:39 -0500

    publish crash

--- src/RegistrationPage.js
+++ src/RegistrationPage.js
@@ -1,8 +1,31 @@
 import React, { useState } from 'react';
-
+import { Input, Checkbox, Button, Message, Card } from 'semantic-ui-react';
+import './RegistrationPage.css';
 const RegistrationPage = () => {
   return (
-    <h1>Registration Page</h1>
+    <div>
+      <div className="topDiv">
+        <h1>Registration Page</h1>
+      </div>
+
+      <div className="medDiv">
+        <div style={{  }}>
+          <div style={{ }}>
+          <Input placeholder="Purdue email" style={{ float: 'center', width: '50%', maxHeight: '45px', fontSize: '20px', marginLeft: '10px' }}
+
+          />
+          <Input placeholder="Password" type="password" style={{ float: 'center', width: '50%', maxHeight: '45px', fontSize: '20px', marginTop: '18px', marginLeft: '10px' }}
+
+          />
+          </div>
+        </div>
+      </div>
+
+      <div>
+      </div>
+    </div>
+
+
   );
 }
 export default RegistrationPage;
