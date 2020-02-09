61f93cd0878ee4a353ff839a8e9aad1a08c8b398
root <root@ip-172-31-22-191.us-east-2.compute.internal>
2020-02-09 06:46:31 +0000

    Fixed always redirect on registration page

--- src/RegistrationPage.js
+++ src/RegistrationPage.js
@@ -12,14 +12,11 @@ const RegistrationPage = () => {
   const [showError, setShowError] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
 
-  const verifyInput = () => {
-
-  }
   return (
     <div>
       <div className="topDiv">
         <img src={ mainLogo } style={{ marginTop: '10px', float: 'center', height: '40px' }} />
-        <h1>Registration Page</h1>
+        <h1>Create an Account</h1>
         <div style={{ float: 'center', maxWidth: '350px' , margin: '0 auto'}}>
           <h4 style={{ color: 'rgba(0, 0, 0, .55)', textAlign: 'center',  float: 'center', marginTop: '17px', marginLeft: '17px', position: 'relative' }}>A simple way to coordinate and connect with students on campus, hassle free.</h4>
         </div>
@@ -28,8 +25,8 @@ const RegistrationPage = () => {
 
       <div className="medDiv" style={{ float: 'center', maxWidth: '700px' , margin: '0 auto'}}>
         <div className="error-message">
-          <Message negative hidden={ !showError } style={{ marginBottom: '35px' }}>
-            <Message.Header style={{ fontFamily: 'Exo 2' }}>Registration Fail</Message.Header>
+          <Message negative hidden={ !showError } style={{ margin: '0 auto', marginBottom: '35px', maxWidth: '500px' }}>
+            <Message.Header style={{ fontFamily: 'Exo 2' }}>Registration Failed</Message.Header>
             <p style={{ fontFamily: 'Exo 2' }}>{ errorMessage}</p>
           </Message>
         </div>
@@ -69,35 +66,38 @@ const RegistrationPage = () => {
 
               if (firstName === "" || lastName === "" || email === "" || password === "" || passwordConfirmation === "") {
                 setShowError(true);
-                setErrorMessage("One or more fields are empty.");
+                setErrorMessage("One or more fields in the form are empty.");
               }else if(password !== passwordConfirmation){
                 setShowError(true);
-                setErrorMessage("Your passwords do not match");
+                setErrorMessage("Your password fields do not match. Please verify that both password field inputs are identical.");
               }else if(password.length < 8){
                 setShowError(true);
                 setErrorMessage("Your password should be at least 8 characters in length.");
               }else if(email.substring(email.indexOf('@')) !== "@purdue.edu"){
                 setShowError(true);
-                setErrorMessage("Your email should be in purdue's domain.");
+                setErrorMessage("Your email should be within Purdue's domain. We are currently only allowing Purdue students to use our platform. Don't worry, we will be expanding our platform in the near future!");
               }else {
                 setShowError(false);
-               // window.location.href = "/register";
-              }
-              const settings = {
-                method : "POST",
-                headers: {
-                  'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ "email": email, "first_name": firstName, "last_name": lastName, "password": password})
-              }
-              const response = await fetch(
-                `http://18.219.112.140:8000/api/v1/register/`, settings
-              );
-              const result = await response.json();
-              console.log("Result: ", result);
-	      console.log("ResultStatus: ", result.status);
-	      if(result.status === "success"){
-		window.location.href = "/";
+
+                const settings = {
+                  method : "POST",
+                  headers: {
+                    'Content-Type': 'application/json'
+                  },
+                  body: JSON.stringify({ "email": email, "first_name": firstName, "last_name": lastName, "password": password})
+                }
+                const response = await fetch(
+                  `http://18.219.112.140:8000/api/v1/register/`, settings
+                );
+                const result = await response.json();
+                console.log("Result: ", result);
+                console.log("ResultStatus: ", result.status);
+                if(result.status === "success"){
+                    window.location.href = "/";
+                } else {
+                  setShowError(true);
+                  setErrorMessage("Our record shows that this Purdue email has already registered an account on Syncopate. In order to reset your password to recover your account, please follow the \"Forgot your password?\" link on the homepage.");
+                }
               }
             }}/>
 
