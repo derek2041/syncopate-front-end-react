480cf99fcf0f76a12ccafdf1b2941312fd1b3842
root <root@ip-172-31-22-191.us-east-2.compute.internal>
2020-02-13 02:51:50 +0000

    Added profile photo upload functionality

--- src/TestLogin.js
+++ src/TestLogin.js
@@ -1,8 +1,28 @@
 import React, { useState, useEffect } from 'react';
+import { Button } from 'semantic-ui-react';
 
 const TestLogin = () => {
   const [loadState, setLoadState] = useState(null);
   const [loadedData, setLoadedData] = useState({});
+  const [avatarURL, setAvatarURL] = useState(null);
+
+  const submitFileUpload = async () => {
+    const formData = new FormData();
+    const fileField = document.querySelector('input[type="file"]');
+    formData.append('avatar', fileField.files[0]);
+
+    const response = await fetch(
+      `http://18.219.112.140:8000/api/v1/upload-avatar/`,
+      {
+        method: 'POST',
+        body: formData,
+        credentials: 'include'
+      }
+    );
+
+    const result = await response.json();
+    console.log(result);
+  }
 
   useEffect(() => {
     async function identifyUser() {
@@ -12,11 +32,22 @@ const TestLogin = () => {
 
         const result = await response.json();
         setLoadedData(result);
-        setLoadState(true);
         console.log(result);
     }
 
+    async function fetchAvatar() {
+      const response = await fetch(
+        `http://18.219.112.140:8000/api/v1/my-avatar/`, { method: 'GET', credentials: 'include'}
+      );
+
+      const result = await response.json();
+      setAvatarURL(result.url);
+      setLoadState(true);
+      console.log(result);
+    }
+
     identifyUser();
+    fetchAvatar();
 
   }, []);
 
@@ -25,11 +56,15 @@ const TestLogin = () => {
       <h1>Loading...</h1>
     );
   } else if (loadState === true) {
+    console.log("url:" + avatarURL);
     return (
       <>
         <h1>Email: {loadedData.email}</h1>
         <h1>F_Name: {loadedData.first_name}</h1>
         <h1>L_Name: {loadedData.last_name}</h1>
+        <img src={ avatarURL } style={{ height: '200px', width: '200px', borderRadius: '50px' }}/>
+        <input type="file" name="avatar" />
+        <Button primary content="Upload Picture" onClick={ submitFileUpload }/>
       </>
     );
   }
