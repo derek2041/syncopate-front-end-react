62eb00d9c00d968ad6d8b078ac50da18ebb2b74d
zhan2539 <zhan2539@purdue.edu>
2020-02-19 20:54:32 +0000

    confirm reset pw page

--- src/ResetPassword2.js
+++ src/ResetPassword2.js
@@ -4,6 +4,10 @@ import mainLogo from './images/1x/Asset 22.png';
 
 
 const ResetPassword2 = () => {
+  const [resetPassword, setResetPassword] = useState("");
+  const [resetPasswordConfirmation, setResetPasswordConfirmation] = useState("");
+  const [showError, setShowError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
   return (
     <div>
       <div style={{paddingTop: '60px', marginBottom: '20px'}} >
@@ -16,7 +20,60 @@ const ResetPassword2 = () => {
       </div>
 
       <div  style={{ float: 'center', maxWidth: '700px' , margin: '0 auto'}}>
+        <div className="error-message">
+          <Message negative hidden={ !showError } style={{ marginBottom: '35px' }}>
+            <Message.Header style={{ fontFamily: 'Exo 2' }}>Reset Password Fail</Message.Header>
+            <p style={{ fontFamily: 'Exo 2' }}>{ errorMessage}</p>
+          </Message>
+        </div>
+        <Input placeholder="New Password" type="password" style={{ float: 'center', width: '50%', maxHeight: '45px', fontSize: '20px', marginTop: '18px', marginLeft: '10px' }}
+        onChange={(event, data) => {
+          setResetPassword(data.value);
+          console.log(data.value);
+        }}
+        />
+        <Input placeholder="Password Confirmation" type="password" style={{ float: 'center', width: '50%', maxHeight: '45px', fontSize: '20px', marginTop: '18px', marginLeft: '10px' }}
+          onChange={(event, data) => {
+            setResetPasswordConfirmation(data.value);
+            console.log(data.value);
+          }}
+        />
+        <Button primary style={{ float: 'center', width: '50%', marginTop: '20px', marginLeft: '10px', borderRadius: '50px', fontSize: '18px' }} content="Submit"
+        onClick={ async () => {
+          if (resetPassword === "" || resetPasswordConfirmation === "") {
+            setShowError(true);
+            setErrorMessage("The password field should not be empty.");
+          }else if(resetPassword !== resetPasswordConfirmation){
+            setShowError(true);
+            setErrorMessage("Your passwords do not match");
+          }else if(resetPassword.length < 8){
+            setShowError(true);
+            setErrorMessage("Your password should be at least 8 characters in length.");
+          }else {
+            setShowError(false);
+           // window.location.href = "/register";
+          }
+          var full_url = window.location.href;
 
+          var token = full_url.substring(full_url.lastIndexOf("/") + 1);
+          console.log(token);
+          const settings = {
+            method : "POST",
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({"password": resetPassword}, {"token" : token})
+          }
+          const response = await fetch(
+            `http://18.219.112.140:8000/api/v1/register/`, settings
+          );
+          const result = await response.json();
+
+          if(result.status === "success"){
+              window.location.href = "/";
+          }
+        }}
+        />
       </div>
 
 
