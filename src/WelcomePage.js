088d4fc57b63ed8d22ff5e79441158ba5c75cf3f
Ubuntu <ubuntu@ip-172-31-22-191.us-east-2.compute.internal>
2020-02-20 05:06:21 +0000

    Added maxwidth for about section

--- src/WelcomePage.js
+++ src/WelcomePage.js
@@ -213,7 +213,7 @@ const WelcomePage = () => {
 
       <div id="outer-footer" className="outer-footer" style={{ paddingBottom: '20px', borderStyle: 'solid', borderWidth: '1px 0px 0px 0px', borderColor: 'gray', background: 'white' }}>
         <h1 style={{ marginTop: '20px', fontSize: '32px' }}>Meet The Team</h1>
-        <Card.Group doubling={ true } stackable={ true } textAlign='center' itemsPerRow={ 6 } style={{ margin: '0 auto', minWidth: '675px' }}>
+        <Card.Group doubling={ true } stackable={ true } textAlign='center' itemsPerRow={ 6 } style={{ margin: '0 auto', minWidth: '675px', maxWidth: '1365px' }}>
           <Card
             image="http://milab.cs.purdue.edu/static/admin/img/none_ava.jpg"
             header='Derek Shu'
