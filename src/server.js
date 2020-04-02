71efa7aa14bc1d79c8b1e91fa676ca5c57ce7bb9
Ubuntu <ubuntu@ip-172-31-22-191.us-east-2.compute.internal>
2020-04-02 22:50:56 +0000

    adding add-message stuff to server.js

--- src/server.js
+++ src/server.js
@@ -9,10 +9,33 @@ io.on("connection", socket => {
   socket.on("subscribeToRoom", room => {
     socket.join(room);
     console.log("Connecting client to room: ", room);
+      
+    socket.on("new message", async function(message) {
+     const settings = {
+       method: "POST",
+       headers: {
+         "Content-Type": "application/json"
+       },
+       credentials: "include",
+       body: JSON.stringify({
+         user_id: message.user_id,
+         group_id: message.group__id,
+         content: message.content,
+         rich_content: null,
+         rich_content_url: null,
+       })
+     };
+     
+     const response = await fetch(
+       `http://18.219.112.140:8000/api/v1/add-message/`,
+       settings
+     );
 
-    socket.on("new message", function(message) {
-      console.log("Pushing " + message + " from " + socket.id + " to clients in room " + room);
-      io.in(room).emit("push to clients", message);
+     const result = await response.json();
+     console.log(result);
+     
+     console.log("Pushing " + message + " from " + socket.id + " to clients in room " + room);
+     io.in(room).emit("push to clients", message);
     });
   });
 });
